51b9951abf2289a71f605dc893dfc8acbfeec3b6
ThakareSakshi <sakshithakare121@gmail.com>
2024-01-07 22:16:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,30 +7,53 @@ import Images from './components/Images';
 
 function App() {
   const [data,setData]=useState([]);
+  const [limit,setLimit]=useState(30);
+  const [height,SetHeight]=useState(1000);
   const apikey="kVeJ9_FubrJNuP7sxiJx4M9Dc49gg5vSRaZMhr2VTj0";
 
 
-  useEffect(()=>{
-    const fetchData=async()=>{
-      try{
-        const response=await axios.get(`https://api.unsplash.com/photos/random/?client_id=${apikey}&count=10`);
-         console.log(response.data)
-         setData(response.data)
-      }
-      catch(e){
-         console.log("error aala",e);
-      }
+  const handleScroll=()=>{
+    const scrolledHeight = window.innerHeight + document.documentElement.scrollTop;
+    const totalHeight = document.documentElement.offsetHeight;
+
+    console.log(scrolledHeight)
+    const threshold = 100;
+
+   
+    if (totalHeight - scrolledHeight < threshold ) {
+      fetchData();
     }
+  }
+
+
+  const fetchData=async()=>{
+    try{
+      const response=await axios.get(`https://api.unsplash.com/photos/random/?client_id=${apikey}&count=${limit}`);
+       console.log(response.data)
+       setData((olddata)=>[...olddata,...response.data])
+    }
+    catch(e){
+       console.log("error aala",e);
+    }
+  }
+
+  useEffect(()=>{
+  
 
     fetchData()
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
      
 
   },[])
 
   return (
-    <div className="App">
+    <div className="App"  >
       <Header/>
-      <div className='images-wrapper'>
+      <div className='images-wrapper' >
         {
           data.map((images)=> {
             return <Images data={images}/>
